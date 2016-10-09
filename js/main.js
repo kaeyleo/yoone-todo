f5059913c44411ef7a5c198db2416aa733fc8c5f
steve liao <mynamelky@gmail.com>
2016-10-09 14:23:11 +0800

    beta

--- js/main.js
+++ js/main.js
@@ -1,82 +1,103 @@
+var model = Model();
+var render = Render();
+render.init();
 
-// svg
-var removeSVG = '<svg viewBox="0 0 16.03125 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><style type="text/css"><![CDATA[@font-face{font-family: ifont; src: url("//at.alicdn.com/t/font_1442373896_4754455.eot?#iefix") format("embedded-opentype"), url("//at.alicdn.com/t/font_1442373896_4754455.woff") format("woff"), url("//at.alicdn.com/t/font_1442373896_4754455.ttf") format("truetype"), url("//at.alicdn.com/t/font_1442373896_4754455.svg#ifont") format("svg");}]]></style></defs><g class="transform-group"><g transform="scale(0.015625, 0.015625)"><path d="M897.091589 173.051311l0 743.40944c0 21.185508-8.832156 40.523949-22.933315 54.602595l-0.067538 0.066515c-14.101159 14.078647-33.612539 22.896476-54.764278 22.896476L203.308406 994.026338c-21.152762 0-40.593534-8.81783-54.694693-22.896476l-0.067538-0.066515-0.069585 0.066515c-14.032598-14.077623-22.862707-33.484625-22.862707-54.670134L125.613883 234.6963c0-14.21577 11.568477-25.834388 25.875321-25.834388 14.306844 0 25.944905 11.618619 25.944905 25.834388l0 681.763428c0 6.900154 2.944052 13.394055 7.736195 18.108426l-0.069585 0.069585c4.724605 4.713348 11.226692 7.653307 18.20871 7.653307l616.018052 0c6.982018 0 13.485129-2.938935 18.20871-7.585769 4.724605-4.714372 7.736195-11.207249 7.736195-18.24555L845.272386 173.051311l-95.973842 0-475.960179 0L25.943859 173.051311c-14.305821 0-25.943882-11.549034-25.943882-25.831318s11.637038-25.833365 25.943882-25.833365l221.517139 0L247.460998 79.288834c0-21.255093 8.761548-40.662095 22.864754-54.740742C284.357326 10.536984 303.797074 1.722224 325.157568 1.722224l372.319727 0c21.3564 0 40.730657 8.747221 54.831816 22.757307l0.067538 0.069585L752.376649 24.479531c14.102183 14.078647 22.864754 33.487695 22.864754 54.810327l0 42.09677 221.517139 0c14.305821 0 25.875321 11.55108 25.875321 25.833365s-11.5695 25.831318-25.875321 25.831318L897.091589 173.051311zM675.845625 293.535383c0-14.283308 11.571546-25.901926 25.87839-25.901926 14.304797 0 25.874297 11.618619 25.874297 25.901926l0 537.839505c0 14.283308-11.570523 25.89988-25.874297 25.89988-14.306844 0-25.87839-11.616572-25.87839-25.89988L675.845625 293.535383zM485.406295 293.535383c0-14.283308 11.636015-25.901926 25.943882-25.901926 14.306844 0 25.875321 11.618619 25.875321 25.901926l0 537.839505c0 14.283308-11.568477 25.89988-25.875321 25.89988-14.307867 0-25.943882-11.616572-25.943882-25.89988L485.406295 293.535383zM295.037573 293.535383c0-14.283308 11.634991-25.901926 25.874297-25.901926 14.304797 0 25.942859 11.618619 25.942859 25.901926l0 537.839505c0 14.283308-11.638061 25.89988-25.942859 25.89988-14.239306 0-25.874297-11.616572-25.874297-25.89988L295.037573 293.535383zM299.282247 121.385605l424.13893 0L723.421177 79.288834c0-7.039323-2.942005-13.531178-7.66661-18.247596l0.068562 0c-4.721535-4.646833-11.158131-7.585769-18.345834-7.585769l-372.319727 0c-7.052626 0-13.487176 2.938935-18.20871 7.653307-4.724605 4.715395-7.66661 11.208273-7.66661 18.179035L299.282247 121.385605z"></path></g></g></svg>';
-var completeSVG = '<svg viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><style type="text/css"><![CDATA[@font-face{font-family: ifont; src: url("//at.alicdn.com/t/font_1442373896_4754455.eot?#iefix") format("embedded-opentype"), url("//at.alicdn.com/t/font_1442373896_4754455.woff") format("woff"), url("//at.alicdn.com/t/font_1442373896_4754455.ttf") format("truetype"), url("//at.alicdn.com/t/font_1442373896_4754455.svg#ifont") format("svg");}]]></style></defs><g class="transform-group"><g transform="scale(0.015625, 0.015625)"><path d="M511.174192 67.649749c-244.689908 0-443.046558 198.358697-443.046558 443.046558 0 244.689908 198.35665 443.046558 443.046558 443.046558 244.686838 0 443.046558-198.35665 443.046558-443.046558C954.22075 266.00947 755.862054 67.649749 511.174192 67.649749zM511.174192 909.437801c-220.22061 0-398.741493-178.522929-398.741493-398.741493s178.520883-398.741493 398.741493-398.741493c220.218564 0 398.741493 178.522929 398.741493 398.741493S731.392756 909.437801 511.174192 909.437801zM708.859553 372.923478 475.452619 606.334505 357.927949 488.807788c-9.389858-9.387811-25.415856-8.580422-35.798321 1.802042-10.379395 10.377348-11.184737 26.405393-1.796926 35.793204l135.981021 135.983067c4.920056 4.916986 11.660574 7.03216 18.457374 6.442736 7.535627 0.639566 15.292288-1.917676 21.057595-7.684006L746.4548 410.518724c10.382465-10.382465 10.382465-27.212782 0-37.595246C736.074382 362.544083 719.240995 362.544083 708.859553 372.923478z"></path></g></g></svg>';
+$('#addBtn').click(function(event) {
+	if($('#addContent').css('display')==='none') {
+		$('#addContent').fadeIn(100);
+		$('#value').focus();
+		//按钮旋转
+		$('#addBtn').css({
+			'transform': 'rotate(-45deg)',
+			'-webkit-transform': 'rotate(-45deg)'
+		});
+		//弹出子按钮
+		$('.okAddBtn').css('bottom', '30%');
+	}else{
+		$('#addContent').fadeOut(200);
+		//按钮旋转
+		$('#addBtn').css({
+			'transform': 'rotate(0deg)',
+			'-webkit-transform': 'rotate(0deg)'
+		});
+		$('.okAddBtn').css('bottom', '4%');
 
-//add task, add input value to the list
-document.getElementById('task-add-btn').addEventListener('click', function() {
-	
-	var text = document.getElementById('task-input').value;
-	text = Trim(text);
-	if (text) {
-		console.log(text);
-		addTask(text);
-		document.getElementById('task-input').value = '';
-	};
+	}		
 });
 
-//Remove the spaces before and after the string
-function Trim(str) { 
-	return str.replace(/(^\s*)|(\s*$)/g, ""); 
+$('.okAddBtn').click(function(event) {
+	var text = $('#value').val();
+	if(text) {
+		model.addTask(text);
+		$('#value').val("");
+		$('#addContent').fadeOut(200);
+		$('#addBtn').css({
+			'transform': 'rotate(0deg)',
+			'-webkit-transform': 'rotate(0deg)'
+		});
+		$('.okAddBtn').css('bottom', '4%');
+		$('.okAddBtn').css('background-color', '#757575');
+	} 
+});
+
+$('#value').bind('input propertychange', function(event) {
+
+		var vinput = $('#value').val();
+		if(vinput!=='') {
+			$('.okAddBtn').css('background-color', '#ff4081');
+		}
+		if(vinput==='') {
+			$('.okAddBtn').css('background-color', '#757575');
+		}
+});
+
+$("#task-list").on("click", ".finish",function() {
+	// $(this).parent().delay(400).fadeOut(300);
+	model.completeTask($(this).parent('.task-item').find('i').text());
+ });
+
+function Model() {
+	var taskItem;
+	var addTask = function(s) {
+		taskItem = [localStorage['sss']];
+		taskItem.push(s);
+		localStorage.setItem('sss', taskItem);
+		render.init();
+	};
+	var completeTask = function(i) {
+		var cpArr = localStorage['sss'].split(',');
+		cpArr.splice(i,1);
+		localStorage.setItem('sss', cpArr);
+		render.init();
+	};
+	return {
+		addTask: addTask,
+		completeTask: completeTask
+	};
 }
 
-//Remove task item
-function removeItem() {
-	var item = this.parentNode.parentNode;
-	var parent = item.parentNode;
-	parent.removeChild(item);
+function Render() {
+	var tasks;
+	var taskTitle;
+	var taskItemHTML='';
+
+	var init = function() {
+		document.getElementById('task-list').innerHTML = '';
+		try {
+			tasks = localStorage['sss'].split(',');
+			for(var i=tasks.length-1; i>0; i--) {
+				taskTitle = tasks[i];
+				taskItemHTML += '<li class="task-item"><span class="task-title">'+taskTitle+'</span><i>'+i+'</i><div class="finish"></div></li>';
+			}
+			document.getElementById('task-list').innerHTML = taskItemHTML;
+			taskItemHTML = '';
+		}catch(e){
+		}
+	}
+	return {
+		init: init
+	};
 }
-
-function completeItem() {
-	var item = this.parentNode.parentNode;
-	var parent = item.parentNode;
-	var id = parent.id;
-	var target = (id==='todo-list') ? document.getElementById('completed-list') : document.getElementById('todo-list');
-
-	parent.removeChild(item);
-	target.insertBefore(item, target.childNodes[0]);
-}
-
-//Add task item
-function addTask(text) {
-
-	var list = document.getElementById('todo-list');
-
-	var item = document.createElement('li');
-	item.classList.add('task-item');
-
-	var itemValue = document.createElement('span');
-	itemValue.classList.add('task-title');
-	itemValue.innerText = text;
-
-	var buttons = document.createElement('div');
-	buttons.classList.add('task-item-buttons');
-
-	var remove = document.createElement('button');
-	remove.classList.add('remove');
-	remove.innerHTML = removeSVG;
-
-	var complete = document.createElement('button');
-	complete.classList.add('complete');
-	complete.innerHTML = completeSVG;
-
-	// li.task-item = span.task-title + div.task-item-buttons(button.remove(svg)+button.complete(svg))
-	buttons.appendChild(remove);
-	buttons.appendChild(complete);
-	item.appendChild(itemValue);
-	item.appendChild(buttons);
-
-	// Click event
-	remove.addEventListener('click', removeItem);
-	complete.addEventListener('click', completeItem);
-
-	list.insertBefore(item, list.childNodes[0]);
-}
-
-var s = localStorage.setItem('text', "Hello!!!!!");
-console.log(localStorage.getItem('text'));
-
-
 
 
 
